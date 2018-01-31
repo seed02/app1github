17bcffd7c6f4b15fe793f64c5197f9d636d4868a
seed02 <sutthisak.sue@mail.pbru.ac.th>
2018-01-31 10:48:43 +0700

    Start

---
+++ app/src/main/java/icepbru/sutthisaksuepsuk/myapplication/logniActivity.java
@@ -0,0 +1,13 @@
+package icepbru.sutthisaksuepsuk.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logni);
+    }
+}
